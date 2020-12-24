9e5f1a00413983e98591096f9a91a1d633053d9a
saif-mhr <saifullah786@hotmail.com>
2020-12-25 00:56:22 +0500

    Add project files.

---
+++ PassingDataContoller_To_view/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PassingDataContoller_To_view.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PassingDataContoller_To_view.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["var1"] = "data come from view";
+            ViewData["var2"] = DateTime.Now.ToLongDateString();
+            string[] arry = { "banana", "apple", "graphes" };
+            ViewData["var3"] = arry;
+
+            ViewData["var4"] = new List<string>()
+            {
+                "cricket",
+               "football",
+               "hockey",
+            };
+
+            Student str = new Student();
+            str.name = "saifullah";
+            str.gender = "Male";
+            str.standard = 5;
+
+            ViewData["var5"] = str;
+            return View();
+        }
+    }
+}
---
+++ PassingDataContoller_To_view/Models/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PassingDataContoller_To_view.Models
+{
+    public class Student
+    {
+        public string name { get; set; }
+        public string gender { get; set; }
+        public int standard { get; set; }
+    }
+}
